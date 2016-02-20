758d98c28e644dd6a7d893b9297fe364250ac5b8
Myra <mxiong@berkeley.edu>
2016-02-19 18:17:10 -0800

    mobile menu

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,4 +8,12 @@ $(document).ready(function() {
 		navigationText: ["<img class='arrow' id='left-arrow' src='assets/img/arrow-left.png'>", "<img class='arrow' id='right-arrow' src='assets/img/arrow-right.png'>"]
 	});
 
+	//Mobile Nav Menu
+
+	$("#menu").click(function() {
+		$(".sections").slideToggle(function () {
+
+		});
+	});
+
 });
